7982f581116346780926677b18be2fee0caff141
Dario <43447240+Leyxargon@users.noreply.github.com>
2021-10-04 22:29:49 +0200

    Aggiunto valore mediano degli oggetti
    - il bot ora ottiene informazioni circa i valori mediani (in platini) degli oggetti monitorati
    - pulizia codice
    - corretti bug minori

--- main.py
+++ main.py
@@ -3,19 +3,25 @@ import colorama
 from sys import argv
 from time import sleep
 
-if len(argv) < 2:
-	oggetto = warframe.cerca('nil')
-else:
-	oggetto = warframe.cerca(argv[1])
+def main(argv):
+	colorama.init(autoreset = True)
 
-min = oggetto.get_prezzo_min()
+	if len(argv) < 2:
+		oggetto = warframe.cerca('nil')
+	else:
+		oggetto = warframe.cerca(argv[1])
 
-while True:
-	print('\u001b[2J', end = '')
-	print(colorama.Style.BRIGHT + oggetto.get_titolo() + colorama.Style.RESET_ALL)
-	oggetto.visualizza_annunci()
-	if oggetto.get_prezzo_min() < min:
-		print(colorama.Fore.RED + oggetto.get_annuncio_min().get('user').get('ingame_name') + ' ha piazzato un nuovo ordine minimo di ' + str(oggetto.get_prezzo_min()) + ' PL' + colorama.Style.RESET_ALL)
-		min = oggetto.get_prezzo_min()
-	sleep(5)
-	oggetto.aggiorna()
+	min = oggetto.get_prezzo_min()
+
+	while True:
+		print('\033c', end = '')
+		print(colorama.Style.BRIGHT + oggetto.get_titolo())
+		oggetto.visualizza_annunci()
+		if oggetto.get_prezzo_min() < min:
+			print(colorama.Back.RED + colorama.Fore.WHITE + oggetto.get_annuncio_min().get('user').get('ingame_name') + ' ha piazzato un nuovo ordine minimo di ' + str(oggetto.get_prezzo_min()) + ' PL')
+			min = oggetto.get_prezzo_min()
+		sleep(5)
+		oggetto.aggiorna()
+
+if __name__ == '__main__':
+	main(argv)
--- warframe.py
+++ warframe.py
@@ -1,16 +1,15 @@
 import requests
 import colorama
-import json
-from pprint import pprint
 
 BASE_URL = 'https://api.warframe.market/v1/'
 
 class Oggetto:
-	def __init__(self, nome):
-		self.id = nome
-		self.titolo = self.__download_titolo(nome)
-		self.annunci = self.__download_annunci(nome)
-		self.annuncio_min = self.__download_annuncio_min(nome)
+	def __init__(self, url):
+		self.url = url
+		self.titolo = self.__download_titolo(url)
+		self.annunci = self.__download_annunci(url)
+		self.annuncio_min = self.__download_annuncio_min(url)
+		self.prezzo_mediano = self.__download_prezzo_mediano(url)
 
 	def __download_titolo(self, query):
 		r = requests.get(BASE_URL + 'items/' + query).json().get('payload').get('item')
@@ -24,6 +23,10 @@ class Oggetto:
 		r = [x for x in requests.get(BASE_URL + 'items/' + query + '/orders').json().get('payload').get('orders') if x.get('order_type') == 'sell' and x.get('user').get('status') == 'ingame']
 		return min(r, key = lambda x : x.get('platinum'))
 
+	def __download_prezzo_mediano(self, query):
+		r = requests.get(BASE_URL + 'items/' + query + '/statistics').json().get('payload').get('statistics_closed').get('90days')
+		return r[len(r) - 1].get('median')
+
 	def get_titolo(self):
 		return self.titolo
 
@@ -34,16 +37,20 @@ class Oggetto:
 		return self.annuncio_min
 
 	def get_prezzo_min(self):
-		return self.annuncio_min['platinum']
+		return self.annuncio_min.get('platinum')
+
+	def get_prezzo_mediano(self):
+		return self.prezzo_mediano
 
 	def visualizza_annunci(self):
-		for i in sorted(self.annunci, key = lambda l: l['platinum']):
-			print(i['user']['ingame_name'] + ': ' + str(i['platinum']) + ' PL')
+		for i in sorted(self.annunci, key = lambda l: l.get('platinum')):
+			print(i.get('user').get('ingame_name') + ': ' + str(i.get('platinum')) + ' PL')
 
-		print('\nPrezzo minimo: ' + str(self.get_prezzo_min()) + ' PL')
+		print('\n' + colorama.Back.BLUE + colorama.Fore.YELLOW + 'Mediana risalente a ieri: ' + str(self.prezzo_mediano) + ' PL')
+		print(colorama.Back.MAGENTA + colorama.Fore.WHITE + 'Prezzo minimo: ' + str(self.get_prezzo_min()) + ' PL')
 
 	def aggiorna(self):
-		tmp = Oggetto(self.id)
+		tmp = Oggetto(self.url)
 		self.annunci = tmp.get_annunci()
 		self.annuncio_min = tmp.get_annuncio_min()
 
@@ -54,9 +61,9 @@ def cerca(query):
 			query = input()
 		r = [x for x in requests.get(BASE_URL + 'items').json().get('payload').get('items') if query.upper() in x.get("item_name").upper()]
 		if len(r) == 1:
-			return Oggetto(r[0].get('url_name'))	
+			return Oggetto(r[0].get('url_name'))
 		elif len(r) > 1:
-			print('Oggetti trovati per ' + colorama.Fore.CYAN + query + colorama.Style.RESET_ALL)
+			print('Oggetti trovati per ' + colorama.Fore.CYAN + query)
 			for i, ris in enumerate(r):
 				print(str(i + 1) + ') ' + ris.get('item_name'))
 			print('\nSelezionare numero oggetto: ')
